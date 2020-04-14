6ebec39253ef8cc1fd3e7dee2a9ef6da0d4ab57f
uilian furtado <uilianbfurtado@hotmail.com>
2020-04-13 21:04:33 -0300

    :sparkles: Introduz feature sub

--- Calculadora.js
+++ Calculadora.js
@@ -3,9 +3,10 @@ function sum(v1, v2) {
 }
 console.log(sum(4, 5));
 
-function sub() {
-  //TODO: Implementar subtração
+function sub(sub1, sub2) {
+  return sub1 - sub2;
 }
+console.log(sub(4, 2));
 
 function mult() {
   //TODO: Implementar multiplicação
